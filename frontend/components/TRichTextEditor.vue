30a24eeee6f6a7707c594848679785c13b73b4f2
Nakorn Sinpadung <nakorndev@gmail.com>
2022-09-08 15:03:12 +0700

    แก้ไขปัญหาของ TinyMCE

--- frontend/components/TRichTextEditor.vue
+++ frontend/components/TRichTextEditor.vue
@@ -3,15 +3,17 @@ import TinyMceEditor from '@tinymce/tinymce-vue'
 </script>
 
 <template>
-  <TinyMceEditor
-    api-key="oosy6smjevo140qa117t4d4c088sbvw55laer5kfqgu8rddy"
-    :init="{
-      height: 500,
-      menubar: false,
-      toolbar:
-        'undo redo | formatselect | bold italic backcolor | \
-        alignleft aligncenter alignright alignjustify | \
-        bullist numlist outdent indent | removeformat | help'
-    }"
-  ></TinyMceEditor>
+  <ClientOnly>
+    <TinyMceEditor
+      api-key="oosy6smjevo140qa117t4d4c088sbvw55laer5kfqgu8rddy"
+      :init="{
+        height: 500,
+        menubar: false,
+        toolbar:
+          'undo redo | formatselect | bold italic backcolor | \
+          alignleft aligncenter alignright alignjustify | \
+          bullist numlist outdent indent | removeformat | help'
+      }"
+    ></TinyMceEditor>
+  </ClientOnly>
 </template>
