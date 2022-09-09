2a2c851982eae51c8845270f4b5fa253f112d2a8
Nakorn Sinpadung <nakorndev@gmail.com>
2022-09-09 17:03:03 +0700

    แก้ไขปัญหาของตัว Client เข้าถึง localhost แทนที่จะไป URL Production

--- frontend/composables/useAxios.ts
+++ frontend/composables/useAxios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default () => {
   const http = axios.create({
-    baseURL: process.env.NODE_ENV === 'production' ? 'http://localhost' : 'http://localhost:3000',
+    baseURL: process.env.NODE_ENV === 'production' ? 'http://178.128.83.133' : 'http://localhost:3000',
     headers: useRequestHeaders(['cookie'])
   })
   return http
