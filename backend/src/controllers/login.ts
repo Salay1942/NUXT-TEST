bb19fbc1ba171fcf18ac0bf90fc2386f7acb7163
Nakorn Sinpadung <nakorndev@gmail.com>
2022-09-09 16:16:51 +0700

    แก้ไขปัญหาการ Build ของ Backend

--- backend/src/controllers/login.ts
+++ backend/src/controllers/login.ts
@@ -1,7 +1,7 @@
 import { compare } from 'bcrypt'
 import { RequestHandler } from 'express'
 import { omit } from 'lodash'
-import { User } from '../generated/prisma-client-js'
+import { User } from '../../generated/prisma-client-js'
 import prisma from '../prisma'
 
 declare module 'express-session' {
