439a7d58660202622211805c51db55687a7198ff
Nakorn Sinpadung <nakorndev@gmail.com>
2022-09-09 16:57:23 +0700

    แก้ไขปัญหาการรัน Production กับ Base URL

--- frontend/composables/useAxios.ts
+++ frontend/composables/useAxios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default () => {
   const http = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: process.env.NODE_ENV === 'production' ? 'http://localhost' : 'http://localhost:3000',
     headers: useRequestHeaders(['cookie'])
   })
   return http
